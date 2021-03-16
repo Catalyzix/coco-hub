c3f46ff3bd6e7f4ec008136f1de2bf748acc7069
falseopx <62042556+falseopx@users.noreply.github.com>
2021-03-16 13:34:31 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,9 @@
+local gamelist = {
+    [286090429] = "Arsenal"
+}
 
+for gameId, coco in pairs(gamelist) do
+    if gameId == game.PlaceId then
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/falseopx/coco-hub/'..coco..'.lua'))()
+    end
+end
